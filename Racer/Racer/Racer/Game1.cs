77cb3b47207fc52e182dc1ae39f909a3b267afa8
Stanley <stanley.yang.sy@gmail.com>
2012-12-01 00:12:53 -0500

    fixed

---
+++ Racer/Racer/Racer/Game1.cs
@@ -0,0 +1,126 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+
+namespace Racer
+{
+    /// <summary>
+    /// This is the main type for your game
+    /// </summary>
+    public class Game1 : Microsoft.Xna.Framework.Game
+    {
+        GraphicsDeviceManager graphics;
+        SpriteBatch spriteBatch;
+        Car Player;
+        startMenu Menu;
+        Rectangle screenRectangle;
+        Boolean start;
+
+        public Game1()
+        {
+            graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+
+            screenRectangle = new Rectangle(0, 0, graphics.PreferredBackBufferWidth, graphics.PreferredBackBufferHeight);
+            start = false;
+            Console.WriteLine("Game1() ran");
+        }
+
+        /// <summary>
+        /// Allows the game to perform any initialization it needs to before starting to run.
+        /// This is where it can query for any required services and load any non-graphic
+        /// related content.  Calling base.Initialize will enumerate through any components
+        /// and initialize them as well.
+        /// </summary>
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+
+            base.Initialize();
+        }
+
+        /// <summary>
+        /// LoadContent will be called once per game and is the place to load
+        /// all of your content.
+        /// </summary>
+        protected override void LoadContent()
+        {
+            // Create a new SpriteBatch, which can be used to draw textures.
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            // TODO: use this.Content to load your game content here
+            Texture2D tempTexture = Content.Load<Texture2D>("ball");
+            Texture2D tempWallTexture = Content.Load<Texture2D>("paddle");
+            Texture2D menuTexture = Content.Load<Texture2D>("startMenu");
+
+            Menu = new startMenu(menuTexture);
+
+            Player = new Car(tempTexture, screenRectangle);
+            
+
+        }
+
+        /// <summary>
+        /// UnloadContent will be called once per game and is the place to unload
+        /// all content.
+        /// </summary>
+        protected override void UnloadContent()
+        {
+            // TODO: Unload any non ContentManager content here
+        }
+
+        /// <summary>
+        /// Allows the game to run logic such as updating the world,
+        /// checking for collisions, gathering input, and playing audio.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Update(GameTime gameTime)
+        {
+            // Allows the game to exit
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed)
+                this.Exit();
+
+            // TODO: Add your update logic here
+
+            if (!start)
+            {
+                start = Menu.Update();
+                Console.WriteLine(start);
+                Texture2D tempTexture = Content.Load<Texture2D>("ball");
+                Player = new Car(tempTexture, screenRectangle);
+            }
+            if (start)
+            {
+                Player.Update();
+            }
+            
+            base.Update(gameTime);
+        }
+
+        /// <summary>
+        /// This is called when the game should draw itself.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.Black);
+
+            // TODO: Add your drawing code here
+            spriteBatch.Begin();
+            
+            Player.Draw(spriteBatch);
+             
+            Menu.Draw(spriteBatch);
+            spriteBatch.End();
+
+            base.Draw(gameTime);
+        }
+    }
+}
---
+++ Racer/Racer/Racer/startMenu.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace Racer
+{
+    class startMenu
+    {
+        Vector2 position;
+        Texture2D texture;
+
+        Boolean start;
+
+        public startMenu(Texture2D texture)
+        {
+            this.texture = texture;
+            this.start = false;
+        }
+
+        public Boolean Update()
+        {
+            KeyboardState keyboardState = Keyboard.GetState();
+
+            if (keyboardState.IsKeyDown(Keys.Enter))
+                start = true;
+            return start;
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            if (!start)
+            {
+                spriteBatch.Draw(texture, position, Color.White);
+            }
+        }
+    }
+}
