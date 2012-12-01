a3699c23df4c61e09f18303422e68e4bd5f74879
Antony Liang <findaliang@gmail.com>
2012-11-30 22:05:11 -0500

    Added new Wall class, and keyboard movement for the player

--- Racer/Racer/Racer/Car.cs
+++ Racer/Racer/Racer/Car.cs
@@ -12,8 +12,13 @@ namespace Racer
     class Car
     {
         Vector2 position;
+        Vector2 motion;
+        float paddleSpeed = 8f;
+
         Texture2D texture;
         Rectangle screenBounds;
+
+        KeyboardState keyboardState;
 
         public Car(Texture2D texture, Rectangle screenBounds)
         {
@@ -26,12 +31,32 @@ namespace Racer
         void StartPosition()
         {
             position.X = (screenBounds.Width - texture.Width) / 2;
-            position.Y = (screenBounds.Height - texture.Height) / 2;
+            position.Y = ((screenBounds.Height - texture.Height) / 2) + 150;
         }
 
         public void Update() 
-        { 
+        {
+            motion = Vector2.Zero;
 
+            keyboardState = Keyboard.GetState();
+
+            if (keyboardState.IsKeyDown(Keys.Left))
+                motion.X = -1;
+            if (keyboardState.IsKeyDown(Keys.Right))
+                motion.X = 1;
+
+            motion.X *= paddleSpeed;
+            position += motion;
+            LockCar();            
+        
+        }
+
+        private void LockCar()
+        {
+            if (position.X < 0)
+                position.X = 0;
+            if (position.X + texture.Width > screenBounds.Width)
+                position.X = screenBounds.Width - texture.Width;
         }
 
         public void Draw(SpriteBatch spriteBatch)
